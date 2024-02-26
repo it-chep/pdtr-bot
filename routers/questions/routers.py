9151812d49087537f18b273f485673f296872165
maksimneceporuk <maxim.necheporuk@yandex.ru>
2024-02-26 19:01:19 +0300

    Fix states

--- routers/questions/routers.py
+++ routers/questions/routers.py
@@ -20,6 +20,8 @@ async def any_callback(callback: CallbackQuery):
     await create_message_log(callback.message, user)
 
     state = redis_client.get_user_state(callback.message.chat.id)
+    if not state:
+        state = await get_last_state(message.chat.id)
     if state and 'question' in state:
         return await send_next_question(callback.message, callback.data, state, user)
     return await get_built_message(callback.message, user)
